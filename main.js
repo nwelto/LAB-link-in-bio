5adcfc2a29775b20be0b851316535ed636eefd18
Nathan <nathopp@gmail.com>
2023-08-09 15:37:54 -0500

    got rid of non working buttons and added new buttons to get to netlify apps i have deployed

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const project2Button = document.getElementById('btn-2');
 const project3Button = document.getElementById('btn-3');
 
 const project1Url = 'https://sortinghatnw.netlify.app';
-const project2Url = 'https://www.example.com/project2';
-const project3Url = 'https://www.example.com/project3';
+const project2Url = 'https://nwlab-pet-adoption.netlify.app/';
+const project3Url = 'https://product-cards-nw.netlify.app/';
 
 project1Button.addEventListener('click', () => {
   window.location.href = project1Url;
