6dc93bf202a753c386e17eb03e874effc29bf3ab
Nathan <nathopp@gmail.com>
2023-08-09 00:21:12 -0500

    Adding more links and made sorting hat functional.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Link in BIO")
+const project1Button = document.getElementById('btn-1');
+const project2Button = document.getElementById('btn-2');
+const project3Button = document.getElementById('btn-3');
+
+const project1Url = 'https://sortinghatnw.netlify.app';
+const project2Url = 'https://www.example.com/project2';
+const project3Url = 'https://www.example.com/project3';
+
+project1Button.addEventListener('click', () => {
+  window.location.href = project1Url;
+});
+
+project2Button.addEventListener('click', () => {
+  window.location.href = project2Url;
+});
+
+project3Button.addEventListener('click', () => {
+  window.location.href = project3Url;
+});
